da4197d157681f28e4a57957264173728776df3f
maria.eugenia <mariugiraldo40@gmail.com>
2024-03-24 17:11:47 -0500

    swagger a las entidades Dentist - Shift

--- cmd/server/handler/dentist.go
+++ cmd/server/handler/dentist.go
@@ -19,7 +19,15 @@ func NewProductHandler(s product.DentistService) *dentistHandler {
 		s: s,
 	}
 }
-
+// GetById godoc
+// swagger:parameters id query
+// @Summary get a dentist
+// @Tags Dentist
+// @Description get dentist by id
+// @Accept json
+// @Produce json
+// @Success 200 {dentist} domain.Dentist
+// @Router /dentists/get/{id} [get]
 func (h *dentistHandler) GetByID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		idParam := c.Param("id")
@@ -38,7 +46,15 @@ func (h *dentistHandler) GetByID() gin.HandlerFunc {
 	}
 
 }
-
+// Post godoc
+// swagger:parameters id query
+// @Summary post a dentist
+// @Tags Dentist
+// @Description create dentist
+// @Accept json
+// @Produce json
+// @Success 200 {dentist} domain.Dentist
+// @Router /dentists [post]
 func (handler *dentistHandler) Post() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var dentist domain.Dentist
@@ -49,14 +65,22 @@ func (handler *dentistHandler) Post() gin.HandlerFunc {
 
 		dentist, err := handler.s.CreateDentist(dentist)
 		if err != nil {
-			c.JSON(500,gin.H{"error": err.Error()})
+			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
 		c.JSON(200, dentist)
 	}
 }
-
+// Put godoc
+// swagger:parameters id query
+// @Summary update a dentist
+// @Tags Dentist
+// @Description update dentist
+// @Accept json
+// @Produce json
+// @Success 200 {dentist} domain.Dentist
+// @Router /dentists [put]
 func (handler *dentistHandler) Put() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var dentist domain.Dentist
@@ -67,14 +91,22 @@ func (handler *dentistHandler) Put() gin.HandlerFunc {
 
 		dentist, err := handler.s.UpdateDentist(dentist)
 		if err != nil {
-			c.JSON(500,gin.H{"error": err.Error()})
+			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
 		c.JSON(200, dentist)
 	}
 }
-
+// Patch godoc
+// swagger:parameters id query
+// @Summary update a field dentist
+// @Tags Dentist
+// @Description update a field dentist
+// @Accept json
+// @Produce json
+// @Success 200 {dentist} domain.Dentist
+// @Router /dentists [patch]
 func (handler *dentistHandler) Patch() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var dentist domain.Dentist
@@ -85,7 +117,7 @@ func (handler *dentistHandler) Patch() gin.HandlerFunc {
 
 		dentist, err := handler.s.PatchDentist(dentist)
 		if err != nil {
-			c.JSON(500,gin.H{"error": err.Error()})
+			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
@@ -93,8 +125,18 @@ func (handler *dentistHandler) Patch() gin.HandlerFunc {
 	}
 }
 
+// DeleteDentist godoc
+// swagger:parameters id query
+// @Summary deletes a dentist
+// @Tags Dentist
+// @Description deletes dentist by id
+// @Accept json
+// @Produce json
+// @Success 200 {dentist} domain.Dentist
+// @Router /dentists/delete/{id} [delete]
 func (h *dentistHandler) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		idParam := c.Param("id")
 		id, err := strconv.Atoi(idParam)
 		if err != nil {
@@ -102,7 +144,7 @@ func (h *dentistHandler) Delete() gin.HandlerFunc {
 			return
 		}
 		dentist, err := h.s.GetByID(id)
-		if err != nil{
+		if err != nil {
 			web.Failure(c, 404, errors.New("dentist not found"))
 			return
 		}
@@ -112,4 +154,3 @@ func (h *dentistHandler) Delete() gin.HandlerFunc {
 	}
 
 }
-
